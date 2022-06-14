057b338a3aed56096703d3d3ef92f1d8942706a7
Sagar Udasi <iamsagar@amazon.com>
2022-06-14 09:37:39 +0530

    Add Basic API Examples and Data Files.

---
+++ apis/credentials.py
@@ -0,0 +1,19 @@
+#Fill your data here.
+
+LOGIN_ID = ""
+CLIENT_ID = ""
+PASSWORD = ""
+
+class MarketFeed:
+    API_KEY = ""
+    SECRET_KEY = ""
+
+class HistoricalData:
+    API_KEY = ""
+    SECRET_KEY = ""
+
+
+class Trading:
+    API_KEY = ""
+    SECRET_KEY = ""
+
